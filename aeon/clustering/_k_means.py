e4260fc821673e3924d3e67632ad0f5f19cd3b31
jevdende <151859454+jevdende@users.noreply.github.com>
2024-05-29 16:34:29 +0200

    Update _k_means.py

--- aeon/clustering/_k_means.py
+++ aeon/clustering/_k_means.py
@@ -9,7 +9,7 @@ from numpy.random import RandomState
 from sklearn.utils import check_random_state
 
 from aeon.clustering.averaging import VALID_BA_METRICS
-from aeon.clustering.averaging._averaging import _resolve_average_callable
+from aeon.clustering.averaging._averaging import _resolve_average_callable, mean_average
 from aeon.clustering.base import BaseClusterer
 from aeon.distances import pairwise_distance
 
